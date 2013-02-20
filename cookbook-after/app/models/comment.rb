9bb08f14517c851fd26a1d4cc348a3d4b303b7ca
David Silver <dsilver@candidatemetrics.com>
2013-02-19 16:54:20 -0800

    setting up

--- cookbook-after/app/models/comment.rb
+++
@@ -1,9 +0,0 @@
-class Comment < ActiveRecord::Base
-  include PublicActivity::Common
-  # tracked except: :update, owner: ->(controller, model) { controller && controller.current_user }
-  
-  attr_accessible :content
-
-  belongs_to :user
-  belongs_to :recipe
-end
